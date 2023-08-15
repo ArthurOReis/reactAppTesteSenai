5a7a0237e5fca3a7b6461bd152e19cd03e970850
ARTHUR OLIVEIRA DOS REIS <arthur_o_reis@estudante.sesisenai.org.br>
2023-08-15 12:16:45 -0300

    Adicionando projeto

---
+++ App.jsx
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.font}>Oi mãe!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  font: {
+    fontSize: 100,
+    fontWeight: 'bold',
+  },
+});
